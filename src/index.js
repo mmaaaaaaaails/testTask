565cf8f2d69b2a3bf17858b290a5a2e4d40dfb5b
Vadim <v.dudko93@gmail.com>
2020-06-01 01:47:31 +0300

    fix: change section team

--- src/index.js
+++ src/index.js
@@ -2,3 +2,32 @@
 import './assets/css/main.css';
 import './assets/scss/main.scss';
 import './js/common';
+
+const team = document.querySelector('.team_block');
+const personDescription = document.querySelector('div.person_description');
+
+function gg() {
+  team.querySelectorAll('.person').forEach((el) => {
+    el.addEventListener('mouseover', () => {
+      personDescription.style.display = 'flex';
+    });
+  });
+}
+gg();
+
+function ggf() {
+  team.querySelectorAll('.person').forEach((el) => {
+    el.addEventListener('mouseout', () => {
+      personDescription.style.display = 'none';
+    });
+  });
+}
+ggf();
+
+// person.addEventListener('mouseover', () => {
+//   personDescription.style.display = 'flex';
+// });
+
+// person.addEventListener('mouseout', () => {
+//   personDescription.style.display = 'none';
+// });
