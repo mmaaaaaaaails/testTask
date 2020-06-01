9243b6929c015884ebbb7cf4f421a351295b6c92
Vadim <v.dudko93@gmail.com>
2020-06-01 20:03:02 +0300

    feat: add popup for section work

--- src/index.js
+++ src/index.js
@@ -3,31 +3,27 @@ import './assets/css/main.css';
 import './assets/scss/main.scss';
 import './js/common';
 
-const team = document.querySelector('.team_block');
-const personDescription = document.querySelector('div.person_description');
+const modal = document.querySelector('.modal');
+const workPicture = document.querySelectorAll('.work_picture');
+const modalContent = document.querySelector('.modal_content');
+const caption = document.querySelector('.modal_caption');
 
-function gg() {
-  team.querySelectorAll('.person').forEach((el) => {
-    el.addEventListener('mouseover', () => {
-      personDescription.style.display = 'flex';
-    });
+workPicture.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    modal.style.display = 'block';
+    modalContent.setAttribute('src', e.target.src);
+    caption.innerHTML = 'pic1';
   });
-}
-gg();
+});
 
-function ggf() {
-  team.querySelectorAll('.person').forEach((el) => {
-    el.addEventListener('mouseout', () => {
-      personDescription.style.display = 'none';
-    });
-  });
-}
-ggf();
 
-// person.addEventListener('mouseover', () => {
-//   personDescription.style.display = 'flex';
-// });
+const close = document.querySelector('.modal_close');
+close.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
 
-// person.addEventListener('mouseout', () => {
-//   personDescription.style.display = 'none';
-// });
+document.addEventListener('click', (event) => {
+  if (event.target.closest('.modal')) {
+    modal.style.display = 'none';
+  }
+});
